c83e613e349af2dff731ed01a49c99d57a17886c
jari <lzyczy11037@gmail.com>
2024-10-05 19:27:40 +0900

    test

--- install.sh
+++ install.sh
@@ -1,5 +1,16 @@
-# berw install
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+# install zsh and set it as default shell
+sudo apt update
+sudo apt install zsh -y
+
+# install brew
+RUN NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+echo >> ~/.zshrc
+echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> ~/.zshrc
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+
+sudo apt update
+sudo apt-get install build-essential
+brew install gcc
 
 # install sheldon
 if ! command -v sheldon &> /dev/null
@@ -7,17 +18,15 @@ then
     sudo curl --proto '=https' -fLsS https://rossmacarthur.github.io/install/crate.sh \
     | sudo bash -s -- --repo rossmacarthur/sheldon --to /usr/local/bin
 fi
-sheldon init --shell zsh
-
-# if zsh is not installed, install it
-if ! command -v zsh &> /dev/null
-then
-    brew install zsh
-fi  
+# sheldon init --shell zsh
 
 # paste symbolic links
 ln -s ~/dotfiles/.zshrc ~
 ln -s ~/dotfiles/.config/sheldon/plugins.toml ~/.config/sheldon
+
+# setup starship
+brew install starship
+echo 'eval "$(starship init zsh)"' >> ~/.zshrc
 
 # set default shell to zsh
 chsh -s $(which zsh)
